9f87cfa00892b5b429422cc64f1f381ce0197962
Robert Corty <rcorty@gmail.com>
2024-09-15 16:18:27 -0500

    add qvalue and time at risk

--- R/do_survival.R
+++ R/do_survival.R
@@ -2,14 +2,14 @@ years_between = function(start, end) {
 	as.numeric(difftime(end, start, units = "days"))/365.24
 }
 
-# TODO
+
 chip_to_ra_survival = function(bl_data,
 															 outcomes,
 															 min_num_events = 100,
 															 min_num_events_w_chip = 10,
 															 ra_types = c('ra', 'spra', 'snra'),
 															 sensspecs = c('sensitive', 'moderate', 'specific'),
-															 chip_types =  names(select(outcomes, starts_with('date_first'))),
+															 chip_types =  names(select(bl_data, starts_with('has_chip'))),
 															 debug = FALSE) {
 
 	bl_data |>
@@ -41,7 +41,6 @@ chip_to_ra_survival = function(bl_data,
 
 				message('Starting ', chip_type, '...')
 
-				# TODO: calculate time at risk, stratified by chip_type, and store that
 				df_for_surv |>
 					group_by(across(chip_type)) |>
 					summarise(n_event = sum(!is.na(.data[[outcome_str]]))) ->
@@ -50,16 +49,25 @@ chip_to_ra_survival = function(bl_data,
 				if (sum(event_counts$n_event) < min_num_events) { next }
 				if (min(event_counts$n_event) < min_num_events_w_chip) { next }
 
+				str_form = glue('time_at_risk ~ {chip_type}')
+				py = pyears(as.formula(str_form), df_for_surv)$pyears
+
 				str_form = glue('Surv(time = time_at_risk, event = event) ~ age_at_biosample + age2 + sex + race + ever_smoker + {chip_type}')
 				fit = coxph(as.formula(str_form), data = df_for_surv)
 				if (debug) { browser() }
 				tidy(fit) |>
-					mutate(ra_type = ra_type,
+					mutate(q.value = qvalue(p.value)$qvalues,
+								 ra_type = ra_type,
 								 sensspec = sensspec,
 								 outcome = outcome_str,
 								 n = nrow(df_for_surv),
-								 n_event = sum(df_for_surv$event),
-								 n_chip = sum(df_for_surv[[chip_type]])) |>
+								 n_chip = sum(df_for_surv[[chip_type]]),
+								 n_event = sum(event_counts$n_event),
+								 n_events_w_chip = min(event_counts$n_event),
+								 time_at_risk_nochip = py['FALSE'],
+								 time_at_risk_chip = py['TRUE']
+
+					) |>
 					filter(str_detect(term, 'chip')) ->
 					toresult
 				results[[paste0(outcome_str, '_', chip_type)]] = toresult

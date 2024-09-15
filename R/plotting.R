305ab9684783055f97bb6dc6093bf1c03a17e64c
Robert Corty <rcorty@gmail.com>
2024-09-15 18:26:34 -0500

    flip the colors so it plots better when nothing is significant

--- R/plotting.R
+++ R/plotting.R
@@ -14,11 +14,11 @@ one_cohort_all_results_plot = function(results, p_or_q = 'p') {
 			ra_type = factor(ra_type, levels = c('ra', 'spra', 'snra'),
 											 labels = c('RA', 'SPRA', 'SNRA')),
 			`p value` = factor(cut(p.value, breaks = c(0, 0.01, 0.05,  1)),
-												 levels = c('(0,0.01]', '(0.01,0.05]', '(0.05,1]'),
-												 labels = c('< 0.01', '< 0.05', 'n.s.')),
+												 levels = rev(c('(0,0.01]', '(0.01,0.05]', '(0.05,1]')),
+												 labels = rev(c('< 0.01', '< 0.05', 'n.s.'))),
 			`q value` = factor(cut(q.value, breaks = c(0, 0.01, 0.05,  1)),
-												 levels = c('(0,0.01]', '(0.01,0.05]', '(0.05,1]'),
-												 labels = c('< 0.01', '< 0.05', 'n.s.'))
+												 levels = rev(c('(0,0.01]', '(0.01,0.05]', '(0.05,1]')),
+												 labels = rev(c('< 0.01', '< 0.05', 'n.s.')))
 		) |>
 		ggplot(mapping = aes(
 			x = exp(estimate),
@@ -29,7 +29,7 @@ one_cohort_all_results_plot = function(results, p_or_q = 'p') {
 		geom_vline(xintercept = 1) +
 		{ if (p_or_q == 'p') geom_pointrange(aes(color = `p value`)) } +
 		{ if (p_or_q == 'q') geom_pointrange(aes(color = `q value`)) } +
-		scale_color_manual(values = c('red', 'blue', 'black')) +
+		scale_color_manual(values = c('black', 'blue', 'red')) +
 		theme_minimal() +
 		labs(x = 'hazard ratio for RA',
 				 y = NULL,

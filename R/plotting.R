68a643630adbe0a1e22a306a163ace7247fde12f
Robert Corty <rcorty@gmail.com>
2024-09-16 08:48:54 -0500

    improve

--- R/plotting.R
+++ R/plotting.R
@@ -1,4 +1,4 @@
-one_cohort_all_results_plot = function(results, p_or_q = 'p') {
+one_cohort_all_results_plot = function(results, title, p_or_q = 'p') {
 
 	stopifnot(p_or_q %in% c('p', 'q'))
 	p_or_q_name = paste0(p_or_q, ' value')
@@ -33,6 +33,46 @@ one_cohort_all_results_plot = function(results, p_or_q = 'p') {
 		theme_minimal() +
 		labs(x = 'hazard ratio for RA',
 				 y = NULL,
-				 title = 'UKB: Hazard ratio for Incident RA') +
+				 title = title) +
 		facet_grid(rows = vars(ra_type, sensspec))
+}
+
+one_cohort_specific_results_plot = function(results, title, p_or_q = 'p') {
+
+	stopifnot(p_or_q %in% c('p', 'q'))
+	p_or_q_name = paste0(p_or_q, ' value')
+
+	results |>
+		filter(sensspec == 'specific') |>
+		mutate(
+			chip_type = factor(str_sub(term, 5, -5),
+												 levels = c('chip', 'chip05', 'chip10', 'chip15',
+												 					 'chip_d3a', 'chip_tet2', 'chip_asxl1'),
+												 labels = c('CHIP', 'CHIP 5%+', 'CHIP 10%+', 'CHIP 15%+',
+												 					 'DNMT3A CHIP', 'TET2 CHIP', 'ASXL1 CHIP')),
+			sensspec = factor(sensspec, levels = c('sensitive', 'moderate', 'specific')),
+			ra_type = factor(ra_type, levels = c('ra', 'spra', 'snra'),
+											 labels = c('RA', 'SPRA', 'SNRA')),
+			`p value` = factor(cut(p.value, breaks = c(0, 0.01, 0.05,  1)),
+												 levels = c('(0,0.01]', '(0.01,0.05]', '(0.05,1]'),
+												 labels = c('< 0.01', '< 0.05', 'n.s.')),
+			`q value` = factor(cut(q.value, breaks = c(0, 0.01, 0.05,  1)),
+												 levels = c('(0,0.01]', '(0.01,0.05]', '(0.05,1]'),
+												 labels = c('< 0.01', '< 0.05', 'n.s.'))
+		) |>
+		ggplot(mapping = aes(
+			x = exp(estimate),
+			y = chip_type,
+			xmin = exp(estimate - 1.96*std.error),
+			xmax = exp(estimate + 1.96*std.error))
+		) +
+		geom_vline(xintercept = 1) +
+		{ if (p_or_q == 'p') geom_pointrange(aes(color = `p value`)) } +
+		{ if (p_or_q == 'q') geom_pointrange(aes(color = `q value`)) } +
+		scale_color_manual(values = c('red', 'blue', 'black'), drop = FALSE) +
+		theme_minimal() +
+		labs(x = 'hazard ratio for RA',
+				 y = NULL,
+				 title = title) +
+		facet_grid(rows = vars(ra_type))
 }

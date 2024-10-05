0e47f50b09796a170bf70846c900cd0b960dd381
Robert Corty <rcorty@gmail.com>
2024-10-04 21:34:00 -0500

    fix analysis

--- R/do_survival.R
+++ R/do_survival.R
@@ -50,54 +50,50 @@ chip_to_ra_survival = function(bl_data,outcomes,
 
 			df_for_survprep |>
 				# remove people with prevalent disease
-				# filter(is.na(.data[[outcome_str]]) | .data[[outcome_str]] > biosample_date) |>
+				filter(is.na(.data[[outcome_str]]) | .data[[outcome_str]] > biosample_date) |>
 				# event = had the disease before censoring
-				# mutate(event = !is.na(.data[[outcome_str]]) & .data[[outcome_str]] < censor_date) |>
-				mutate(event = !is.na(.data[[outcome_str]])) ->
+				mutate(event = !is.na(.data[[outcome_str]]) & .data[[outcome_str]] < censor_date) |>
 				# time at risk definition
-				# mutate(time_at_risk = case_when(event ~ years_between(biosample_date, .data[[outcome_str]]),
-				# 																.default = years_between(biosample_date, censor_date))) ->
+				mutate(time_at_risk = case_when(event ~ years_between(biosample_date, .data[[outcome_str]]),
+																				.default = years_between(biosample_date, censor_date))) ->
 				df_for_surv
 
 			for(chip_type in chip_types) {
 
 				message('Starting ', chip_type, '...')
 
-				# df_for_surv |>
-				# 	group_by(across(chip_type)) |>
-				# 	summarise(
-				# 		n_event = sum(!is.na(.data[[outcome_str]])),
-				# 		median_fu = median(time_at_risk)) |>
-				# 	arrange({{chip_type}}) ->
-				# 	event_counts
+				df_for_surv |>
+					group_by(across(chip_type)) |>
+					summarise(
+						n_event = sum(!is.na(.data[[outcome_str]])),
+						median_fu = median(time_at_risk)) |>
+					arrange({{chip_type}}) ->
+					event_counts
 
-				# if (sum(event_counts$n_event) < min_num_events) { next }
-				# if (min(event_counts$n_event) < min_num_events_w_chip) { next }
+				if (sum(event_counts$n_event) < min_num_events) { next }
+				if (min(event_counts$n_event) < min_num_events_w_chip) { next }
 
-				# str_form = glue('time_at_risk ~ {chip_type}')
-				# py = pyears(as.formula(str_form), df_for_surv)$pyears
+				str_form = glue('time_at_risk ~ {chip_type}')
+				py = pyears(as.formula(str_form), df_for_surv)$pyears
 
-				# str_form = glue('Surv(time = time_at_risk, event = event) ~ age_at_biosample + age2 + sex + race + ever_smoker + {chip_type}')
-				# fit = coxph(as.formula(str_form), data = df_for_surv)
-
-				str_form = glue('event ~ age_at_biosample + age2 + sex + race + ever_smoker + {chip_type}')
-				fit = glm(formula = as.formula(str_form), data = df_for_surv, family = 'binomial')
+				str_form = glue('Surv(time = time_at_risk, event = event) ~ age_at_biosample + age2 + sex + race + ever_smoker + {chip_type}')
+				fit = coxph(as.formula(str_form), data = df_for_surv)
 
 				if (debug) { browser() }
 				tidy(fit) |>
 					mutate(q.value = qvalue(p.value, lambda = 0)$qvalues,
 								 ra_type = ra_type,
 								 sensspec = sensspec,
-								 outcome = outcome_str
-								 # n_nochip = nrow(df_for_surv) - sum(df_for_surv[[chip_type]]),
-								 # n_chip = sum(df_for_surv[[chip_type]]),
-								 # n_events_nochip = event_counts$n_event[1],
-								 # n_events_w_chip = event_counts$n_event[2],
-								 # time_at_risk_nochip = py['FALSE'],
-								 # time_at_risk_chip = py['TRUE'],
-								 # median_fu = median(df_for_surv$time_at_risk),
-								 # median_fu_nochip = event_counts$median_fu[1],
-								 # median_fu_chip = event_counts$median_fu[2]
+								 outcome = outcome_str,
+								 n_nochip = nrow(df_for_surv) - sum(df_for_surv[[chip_type]]),
+								 n_chip = sum(df_for_surv[[chip_type]]),
+								 n_events_nochip = event_counts$n_event[1],
+								 n_events_w_chip = event_counts$n_event[2],
+								 time_at_risk_nochip = py['FALSE'],
+								 time_at_risk_chip = py['TRUE'],
+								 median_fu = median(df_for_surv$time_at_risk),
+								 median_fu_nochip = event_counts$median_fu[1],
+								 median_fu_chip = event_counts$median_fu[2]
 					) |>
 					filter(str_detect(term, 'chip')) ->
 					toresult
